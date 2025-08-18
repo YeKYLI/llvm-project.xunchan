dada4b1172040329a6bf29a2ba207f33a99c20fd
Wind <wind@dota2.com>
2025-08-18 22:32:51 +0800

    xxx

---
+++ candy.sh
@@ -0,0 +1,10 @@
+cd build
+
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=ON \
+   -DLLVM_TARGETS_TO_BUILD="Native;NVPTX" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON
+
+cmake --build . --target check-mlir
